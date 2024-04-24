cda8dc171399d62f2893390a1bebe5a3c046ea14
Raykrad <109039576+Raykrad@users.noreply.github.com>
2024-04-24 21:16:25 +0300

    Add files via upload

---
+++ external/install_external.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+git clone https://github.com/g-truc/glm.git
+echo 'GLM'
+
+git clone https://github.com/glfw/glfw.git
+echo 'GLFW'
+
+git clone https://github.com/ocornut/imgui.git
+echo 'ImGui'
+
+echo 'Installation is over'
+
